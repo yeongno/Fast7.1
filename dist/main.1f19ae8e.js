bc5afc3560c189e1d1b052e06acf70cffa6b63c3
yeongno <dldudsh83@gmail.com>
2022-03-24 03:23:55 +0900

    테마 색상 커스터마이징

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -6967,7 +6967,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54494" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57942" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
